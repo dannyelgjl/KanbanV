4fdf7260f48fbaeb0fde69d152d9cb524fbf0ae7
dannyelgjl <dannyelgjl@gmail.com>
2020-03-25 19:41:14 -0300

    criando styles e estrutura da page de tarefas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import GlobalStyle from "./styles/global.js";
+
+import Board from "./components/Board";
+
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      {" "}
-      <h1>ok</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+
+      <Board />
+    </>
   );
 }
 
---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container, Label } from "./styles";
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1" />
+      </header>
+      <p>Fazer integração completa da aplicação</p>
+      <img src="https://avatars3.githubusercontent.com/u/54491980?v=4" alt="" />
+    </Container>
+  );
+}
