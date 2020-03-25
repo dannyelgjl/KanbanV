d3d9b3f904e41dc233cfff39c3b90d94201009e5
dannyelgjl <dannyelgjl@gmail.com>
2020-03-25 20:07:42 -0300

    colunas de tarefas estilizadas

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,7 +6,7 @@ export default function Card() {
   return (
     <Container>
       <header>
-        <Label color="#7159c1" />
+        <Label color="#040404" />
       </header>
       <p>Fazer integração completa da aplicação</p>
       <img src="https://avatars3.githubusercontent.com/u/54491980?v=4" alt="" />
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   height: 80px;
   padding: 0 30px;
-  background: #333;
+  background: #040404;
   color: #fff;
 
   display: flex;
