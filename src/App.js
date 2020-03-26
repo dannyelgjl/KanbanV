22738bd9ba73ecd595e4ade6ba2ca23658700ba1
dannyelgjl <dannyelgjl@gmail.com>
2020-03-25 22:15:50 -0300

    funcionalidade de arrastar cards para outras colunas ok

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import { DndProvider } from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
 import GlobalStyle from "./styles/global.js";
 
-import Board from "./components/Board";
-
 import Header from "./components/Header";
+import Board from "./components/Board";
 
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
-      <GlobalStyle />
       <Header />
 
       <Board />
+      <GlobalStyle />
     </DndProvider>
   );
 }
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -9,25 +9,21 @@ export const Container = styled.div`
   box-shadow: 0 1px 4px 0 rgba(192, 208, 230, 0.8);
   border-top: 20px solid rgba(230, 236, 245, 0.4);
   cursor: grab;
-
   header {
     position: absolute;
     top: -22px;
     left: 15px;
   }
-
   p {
     font-weight: 500;
     line-height: 20px;
   }
-
   img {
     width: 24px;
     height: 24px;
     border-radius: 2px;
     margin-top: 5px;
   }
-
   ${props =>
     props.isDragging &&
     css`
@@ -37,10 +33,9 @@ export const Container = styled.div`
       background: transparent;
       box-shadow: none;
       cursor: grabbing;
-
       p,
       img,
-      span {
+      header {
         opacity: 0;
       }
     `}
